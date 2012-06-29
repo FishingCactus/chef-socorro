31772427e6142cca89d7975484d7f5b792520990
Michael Delva <michael.delva@gmail.com>
2012-06-29 13:03:57 +0200

    metadata update

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,16 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README'))
 version           "1.0.0"
 
 recipe "socorro", "Installs and configures a complete socorro server"
-recipe "haproxy::app_lb", "Installs and configures haproxy by searching for nodes of a particular role"
+recipe "socorro::api", "Installs socorro API"
+recipe "socorro::collector", "Installs socorro collector"
+recipe "socorro::crashmover", "Installs socorro crash mover"
+recipe "socorro::monitor", "Installs socorro monitor"
+recipe "socorro::processor", "Installs socorro processor"
+recipe "socorro::ui", "Installs socorro Web UI"
+
+%w{ apt build-essential java git rsyslog subversion python ant cron memcached php apache2" }.each do |cb|
+  depends cb
+end
 
 %w{ debian ubuntu }.each do |os|
   supports os
