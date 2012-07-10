c8c6b8b9359364a636e70c58066d029a0fdca4a3
Michael Delva <michael.delva@gmail.com>
2012-07-10 18:16:02 +0200

    PostgreSQL 9.1 is not supported by now... back to 9.0 :(

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -1,22 +1,50 @@
 include_recipe "chef-socorro::base"
 
+# require_recipe "postgresql::server"
+
+# package "postgresql-plperl" do
+#   action :install
+# end
+
+# package "postgresql-contrib" do
+#   action :install
+# end
+
+# cookbook_file "#{node[:postgresql][:dir]}/postgresql.conf" do
+#   source "postgresql/postgresql.conf" 
+#   owner "postgres"
+#   group "postgres"
+#   mode "0644"
+
+#   notifies :restart, "service[postgresql]"
+# end
+
+execute "Add PostgreSQL repository" do
+  command "/usr/bin/sudo /usr/bin/add-apt-repository ppa:pitti/postgresql"
+  creates "/etc/apt/sources.list.d/pitti-postgresql-lucid.list"
+  user "root"
+  action :run
+end
+
+execute "Update PostgreSQL repository" do
+  command "/usr/bin/apt-get update && touch /tmp/update-postgres-ppa"
+  creates "/tmp/update-postgres-ppa"
+  user "root"
+  action :run
+end
+
+package "postgresql-9.0" do
+  action :install
+end
+
 require_recipe "postgresql::server"
 
-package "postgresql-plperl" do
+package "postgresql-plperl-9.0" do
   action :install
 end
 
-package "postgresql-contrib" do
+package "postgresql-contrib-9.0" do
   action :install
-end
-
-cookbook_file "#{node[:postgresql][:dir]}/postgresql.conf" do
-  source "postgresql/postgresql.conf" 
-  owner "postgres"
-  group "postgres"
-  mode "0644"
-
-  notifies :restart, "service[postgresql]"
 end
 
 execute "Setup PostgreSQL database" do
